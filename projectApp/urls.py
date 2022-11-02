fa71920b49e585c651447e2520c058ed2e4af3d4
asifhasan014 <asifhasan014@gmail.com>
2022-11-02 12:38:46 +0600

    add static,media and template file in sop generator

--- projectApp/urls.py
+++ projectApp/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+
+from projectApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('projectApp.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('',views.index, name="homepage"),
+]
