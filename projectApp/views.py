85ab6862dcc48ff26156a85036cc8ff0d0cc0828
asifhasan014 <asifhasan014@gmail.com>
2022-12-12 22:04:54 +0600

    ui part is ready for development

--- projectApp/views.py
+++ projectApp/views.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+
 from django.shortcuts import render
 import logging
 
@@ -5,5 +8,12 @@ logger = logging.getLogger(__name__)
 
 
 def index(request):
-    logger.info('index page is apear...')
+    # date_str = '2022-11-21 00:00:00:000'
+    # date_obj = datetime.strptime(date_str,'%d/%m/%y %H:%M:%S').time()
+    # print(date_obj)
+    # mylist = ["2022-11-10 00:00:00:000", "2022-11-11 00:00:00:000", "2022-11-12 00:00:00:000"]
+    # for x in mylist:
+    #     print("Date time formate ...")
+    #     print(x)
+    # logger.info('index page is apear...')
     return render(request, 'index.html')
